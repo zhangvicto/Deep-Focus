043cdd14f58dea73e3b0b2af425f59a825342a39
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-03-06 15:37:43 -0500

    update

--- script.js
+++ script.js
@@ -17,14 +17,10 @@ init = function main() {
 
 init();
 
-
 //Switch on/off
-var checkbox = document.querySelector("input[name=checkbox]");
-
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    console.log("Checkbox is checked..");
-  } else {
-    console.log("Checkbox is not checked..");
-  }
-});
+var checkbox1 = document.querySelector("#checkbox1").checked;
+  
+if(checkbox1) 
+    console.log('Checked');
+else
+    console.log('Unchecked');
