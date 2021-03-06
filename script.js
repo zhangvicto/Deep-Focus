8909fdf831efa619915a0232b26f90ff54d58553
Richard <richard.phan01@gmail.com>
2021-03-06 11:14:29 -0500

    updated script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
+findAllURL = function changeAllURL(text){
+    var current = window.location.href;
+    if(current.startsWith(text)){
+      document.documentElement.innerHTML = '';
+      document.documentElement.innerHTML = 'Domain is blocked';
+      document.documentElement.scrollTop = 0;
+    }
+  }
+
+  findAllURL("https://www.facebook.com/");
+  findAllURL("https://www.youtube.com/");
